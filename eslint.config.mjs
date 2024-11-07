06cfcb57d11657a1306d18dc59a60260ff019bc9
刘演 <79697331+ovlineen@users.noreply.github.com>
2024-11-08 00:45:59 +0800

    feat: modified the issue regarding jest configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,25 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",  // 自动检测 React 的版本
+      },
+    },
+    rules: {
+      // 禁用 @typescript-eslint/triple-slash-reference 规则
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
 ];
